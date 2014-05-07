dbba9fa3c599afac6759d283b65002ced01c700b
j4cksw <ninjaadvance@gmail.com>
2014-05-07 15:54:34 +0700

    Update Player_spec

--- spec/Player_spec.lua
+++ spec/Player_spec.lua
@@ -1,7 +1,44 @@
 describe("Player", function()
-  it("should create player image sheet.")
+  Player = {
+    new = function()
+      local playerImageSheet = display.newImageSheet("img/player.png")
+      local playerSprite = display.newSprite(playerImageSheet, {
+        {name="run",
+         start=1,
+         count=1,
+        }
+       })
+      return playerSprite
+    end
+  }
   
-  it("should create player sprite.")
+  local playerImageSheet = {}
   
-  it("should set sequence to run.")
+  display = {
+    newImageSheet = function()
+      return playerImageSheet
+    end
+  }
+  spy.on(display, "newImageSheet")
+  stub(display, "newSprite")
+  
+  it("should create Player's image sheet.", function()
+    Player:new()
+    
+    assert.stub(display.newImageSheet).was_called_with("img/player.png")
+  end)
+  
+  it("should create Player's sprite.", function()
+    Player:new()
+    
+    assert.stub(display.newSprite)
+      .was_called_with(playerImageSheet,
+       {
+        {name="run",
+         start=1,
+         count=1,
+        }
+       })
+  end)
+  
 end)
