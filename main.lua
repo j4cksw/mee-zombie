a08b6664d02c619dd5a9920b55916547ecad578b
j4cksw <ninjaadvance@gmail.com>
2014-05-09 11:41:05 +0700

    Add script for test player sprite in main

--- main.lua
+++ main.lua
@@ -4,6 +4,9 @@ display.setStatusBar( display.HiddenStatusBar )
 Player = require("Player")
 
 local player = Player:new()
---player:walk()
-player:run()
+player:walk()
 player:setPosition(display.contentCenterX, display.contentCenterY)
+
+timer.performWithDelay(2000, function()
+  player:run()
+end)
