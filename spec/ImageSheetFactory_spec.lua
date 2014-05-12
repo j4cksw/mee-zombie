288376b0af00f3faabe265a1ef7f529e997b1df0
j4cksw <ninjaadvance@gmail.com>
2014-05-12 20:01:59 +0700

    Add ImageSheetFactory_spec

---
+++ spec/ImageSheetFactory_spec.lua
@@ -0,0 +1,53 @@
+describe("ImageSheetFactory", function()
+
+    local fakeImageSheet = {}
+
+    ImageSheetFactory = {
+
+        createFromImageSheetData = function(imageSheetData)
+          local result = graphics.newImageSheet(imageSheetData.path, imageSheetData.options)
+          return result
+        end
+    }
+
+    setup(function()
+      graphics = {
+        newImageSheet = function(...)
+          return fakeImageSheet
+        end
+      }
+      spy.on(graphics, "newImageSheet")
+
+    end)
+
+    it("should create image sheet from graphics module", function()
+      local fakeImageSheetData = {
+        path = "path/to/imageSheet",
+        options = {
+          width=10,
+          height=20,
+          numFrames=100
+        }
+      }
+
+      ImageSheetFactory.createFromImageSheetData(fakeImageSheetData)
+
+      assert.stub(graphics.newImageSheet).was_called_with(fakeImageSheetData.path,
+        fakeImageSheetData.options)
+    end)
+
+    it("should return an imageSheet instance", function()
+      local fakeImageSheetData = {
+        path = "path/to/imageSheet",
+        options = {
+          width=10,
+          height=20,
+          numFrames=100
+        }
+      }
+
+      local result = ImageSheetFactory.createFromImageSheetData(fakeImageSheetData)
+
+      assert.are.equal(result, fakeImageSheet)
+    end)
+end)
