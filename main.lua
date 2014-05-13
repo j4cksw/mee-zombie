06b48ed0231340c49818e0b1e02062cfc79e0b8b
j4cksw <ninjaadvance@gmail.com>
2014-05-13 18:45:51 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,10 +3,13 @@ display.setStatusBar( display.HiddenStatusBar )
 
 Player = require("scripts.Player")
 
-local player = Player:new()
-player:walk()
-player:setPosition(display.contentCenterX, display.contentCenterY)
+--local player = Player:new()
+--player:walk()
+--player:setPosition(display.contentCenterX, display.contentCenterY)
+--
+--timer.performWithDelay(2000, function()
+--  player:run()
+--end)
 
-timer.performWithDelay(2000, function()
-  player:run()
-end)
+GameInitializer = require("scripts.GameInitializer")
+GameInitializer.initialize()
