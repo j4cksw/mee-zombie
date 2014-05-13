1e52419b2641b1db86ac0fffea33a2973f56388e
j4cksw <ninjaadvance@gmail.com>
2014-05-13 19:01:50 +0700

    Romove unused comments in main.

--- main.lua
+++ main.lua
@@ -1,15 +1,5 @@
 
 display.setStatusBar( display.HiddenStatusBar )
 
-Player = require("scripts.Player")
-
---local player = Player:new()
---player:walk()
---player:setPosition(display.contentCenterX, display.contentCenterY)
---
---timer.performWithDelay(2000, function()
---  player:run()
---end)
-
 GameInitializer = require("scripts.GameInitializer")
 GameInitializer.initialize()
