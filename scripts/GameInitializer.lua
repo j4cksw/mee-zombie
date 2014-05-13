671707a05e0566920665537f2d279113fb98be83
j4cksw <ninjaadvance@gmail.com>
2014-05-13 19:00:44 +0700

    Add missing statement in GameInitializer

--- scripts/GameInitializer.lua
+++ scripts/GameInitializer.lua
@@ -3,13 +3,15 @@ local GameInitializer = {}
 ImageSheetFactory = ImageSheetFactory or require("scripts.ImageSheetFactory")
 SpriteFactory = SpriteFactory or require("scripts.SpriteFactory")
 PlayerImageSheetConfig = PlayerImageSheetConfig or require("config.PlayerImageSheetConfig")
-PlayerSpriteSequenceData = PlayerImageSheetConfig or require("config.PlayerSpriteSequenceData")
+PlayerSpriteSequenceData = PlayerSpriteSequenceData or require("config.PlayerSpriteSequenceData")
 SpritePositioner = SpritePositioner or require("scripts.SpritePositioner")
+SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
 
 function GameInitializer.initialize()
   local playerImageSheet = ImageSheetFactory.createFromImageSheetData(PlayerImageSheetConfig)
   local playerSprite = SpriteFactory.createFromImageSheet(playerImageSheet, PlayerSpriteSequenceData)
   SpritePositioner.setPosition(playerSprite, display.contentCenterX, display.contentCenterY)
+  SpriteSequenceTransition.toSequence(playerSprite, "walk")
 end
 
 return GameInitializer
