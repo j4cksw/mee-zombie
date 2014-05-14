11af4dc8a1d0a5d9d38b644a47a6e0b016aa272e
j4cksw <ninjaadvance@gmail.com>
2014-05-14 20:09:49 +0700

    Implemented first test of PlayerInitializer

--- scripts/PlayerInitializer.lua
+++ scripts/PlayerInitializer.lua
@@ -1,2 +1,10 @@
 local PlayerInitializer = {}
+
+SpriteFactory = SpriteFactory or require("scripts.SpriteFactory")
+
+function PlayerInitializer.initialize()
+  SpriteFactory.createFromImageSheet(ImageSheetsTable["bear_zombie"],
+    SpriteSequenceData["bear_zombie"])
+end
+
 return PlayerInitializer
--- spec/PlayerInitializer_spec.lua
+++ spec/PlayerInitializer_spec.lua
@@ -1,14 +1,46 @@
 describe("PlayerInitializer", function()
+  local fakeBearSprite = {}
+  
   setup(function()
+    SpriteFactory = {
+      createFromImageSheet = function(...)
+        return fakeBearSprite
+      end
+    }
+    spy.on(SpriteFactory, "createFromImageSheet")
+
+    ImageSheetsTable = {
+      ["bear_zombe"] = {}
+    }
+    
+    SpriteSequenceData = {
+      ["bear-zombie"] = {
+        {name="walk"}
+      }
+    }
+
+    SpritePositioner = {}
+    stub(SpritePositioner, "setPosition")
+
+    display = {
+      contentCenterX=500,
+      contentCenterY=500
+    }
+
+    SpriteSequenceTransition = {}
+    stub(SpriteSequenceTransition, "toSequence")
+
     PlayerInitializer = require("scripts.PlayerInitializer")
   end)
-  
+
   it("should create  bear sprite", function()
     PlayerInitializer.initialize()
-    
+
     assert.stub(SpriteFactory.createFromImageSheet)
       .was_called_with(ImageSheetsTable["bear_zombie"],
         SpriteSequenceData["bear_zombie"])
   end)
-  
+
+
+
 end)
