0dd5efdfd821d7421cc4077337014ba53da2a5b9
j4cksw <ninjaadvance@gmail.com>
2014-05-14 18:19:49 +0700

    Add ImageSheetsData

---
+++ config/ImageSheetsData.lua
@@ -0,0 +1,18 @@
+ImageSheetsData = {
+  ["bear_zombie"] = {
+    path="img/sprite/bear_zombie.png",
+    options={
+      width=400,
+      height=400,
+      numFrames=17
+    }
+  },
+  ["floor"] = {
+    path="img/sprite/floor.png",
+    options={
+      width=128,
+      height=128,
+      numFrames=2
+    }
+  }
+}
