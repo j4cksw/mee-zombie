ff3303d3cf6adafc5d6f8ae6b620c32ec0417bcb
j4cksw <ninjaadvance@gmail.com>
2014-05-15 14:56:43 +0700

    Update acquiring width and height of floor sprite

--- scripts/FloorBuilder.lua
+++ scripts/FloorBuilder.lua
@@ -1,17 +1,20 @@
 local FloorBuilder = {}
 
+ImageSheetsData = ImageSheetsData or require("config.ImageSheetsData")
 SpriteInitializer = SpriteInitializer or require("scripts.Spriteinitializer")
 
 function FloorBuilder.build()
-
-  local floorPieceHeight=128
+  
+  local floorSpriteOptions = ImageSheetsData["floor"].options 
+  local floorPieceWidth = floorSpriteOptions.width
+  local floorPieceHeight = floorSpriteOptions.height
 
   local floorChunkPattern = {"body", "top"}
 
   for key, value in pairs(floorChunkPattern) do
     SpriteInitializer.initializeByData({
       name="floor",
-      x=128/2,
+      x=floorPieceWidth/2,
       y=display.viewableContentHeight- (floorPieceHeight*key) + (floorPieceHeight/2),
       sequence=value
     })
--- spec/FloorBuilder_spec.lua
+++ spec/FloorBuilder_spec.lua
@@ -1,20 +1,29 @@
 describe("FloorBuilder", function()
   local FloorBuilder
-  
+
   setup(function()
     SpriteInitializer = {}
     stub(SpriteInitializer, "initializeByData")
-    
+
     display = {
       viewableContentHeight=1536
     }
-    
+
+    ImageSheetsData = {
+      ["floor"] = {
+        options={
+          width=128,
+          height=128
+        }
+      }
+    }
+
     FloorBuilder = require("scripts.FloorBuilder")
   end)
-  
+
   it("should create the first floor", function()
     FloorBuilder.build()
-    
+
     assert.stub(SpriteInitializer.initializeByData).was_called_with({
       name="floor",
       x=64,
@@ -22,10 +31,10 @@ describe("FloorBuilder", function()
       sequence="top"
     })
   end)
-  
+
   it("should create the second floor", function()
     FloorBuilder.build()
-    
+
     assert.stub(SpriteInitializer.initializeByData).was_called_with({
       name="floor",
       x=64,
