82cd6f7cf8351e9620a8fa8ab842aa2ba2e23706
j4cksw <ninjaadvance@gmail.com>
2014-05-15 11:03:41 +0700

    Implemented SpriteInitializer

--- scripts/PlayerInitializer.lua
+++ scripts/PlayerInitializer.lua
@@ -3,7 +3,6 @@ local PlayerInitializer = {}
 SpriteFactory = SpriteFactory or require("scripts.SpriteFactory")
 SpritePositioner = SpritePositioner or require("scripts.SpritePositioner")
 SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
-SpriteSequenceData = SpriteSequenceData or require("config.SpriteSequenceData")
 
 local PLAYER_SPRITE = "bear_zombie"
 
--- scripts/SpriteInitializer.lua
+++ scripts/SpriteInitializer.lua
@@ -1,6 +1,13 @@
 SpriteInitializer = {}
 
-function SpriteInitializer.initializeByData(spriteInitializeData)
+SpriteFactory = SpriteFactory or require("scripts.SpriteFactory")
+SpritePositioner = SpritePositioner or require("scripts.SpritePositioner")
+SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
+
+function SpriteInitializer.initializeByData(initializeData)
+  local sprite = SpriteFactory.createFromImageSheetName(initializeData.name)
+  SpritePositioner.setPosition(sprite, initializeData.x, initializeData.y)
+  SpriteSequenceTransition.toSequence(sprite, initializeData.sequence)
 end
 
 return SpriteInitializer
