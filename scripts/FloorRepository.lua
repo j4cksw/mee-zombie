66206f0617aa456a9aab2afa49618e8bb6111cb4
j4cksw <ninjaadvance@gmail.com>
2014-05-18 16:09:46 +0700

    Implemented function FloorRepository.getFloorGroup

--- scripts/FloorRepository.lua
+++ scripts/FloorRepository.lua
@@ -1,9 +1,13 @@
 local FloorRepository = {}
 
+FloorRepository.floorGroup = nil
+
 function FloorRepository.setFloorGroup(floorGroup)
+  FloorRepository.floorGroup = floorGroup
 end
 
 function FloorRepository.getFloorGroup()
+  return FloorRepository.floorGroup
 end
 
 return FloorRepository
