109da8a240854cc2284547b093e975a514713b58
j4cksw <ninjaadvance@gmail.com>
2014-05-18 16:35:56 +0700

    Insert floor chunk into floor group

--- scripts/FloorBuilder.lua
+++ scripts/FloorBuilder.lua
@@ -4,11 +4,11 @@ FloorChunkBuilder = FloorChunkBuilder or require("scripts.FloorChunkBuilder")
 FloorPatterns = FloorPatterns or require("config.FloorPatterns")
 
 function FloorBuilder.build()
-  display.newGroup()
+  local floorGroup = display.newGroup()
   
   for horizontalOffset = 1,16 do
-    FloorChunkBuilder.buildFromPatternAndVerticalOffset(FloorPatterns[1],
-      horizontalOffset)
+    floorGroup:insert(FloorChunkBuilder.buildFromPatternAndVerticalOffset(FloorPatterns[1],
+      horizontalOffset))
   end
 end
 
--- spec/FloorBuilder_spec.lua
+++ spec/FloorBuilder_spec.lua
@@ -1,16 +1,27 @@
 describe("FloorBuilder", function()
   local FloorBuilder
 
+  local fakeFloorGroup = {}
+
+  local fakeCreatedFloorChunk = {}
+
   setup(function()
+    
+    stub(fakeFloorGroup, "insert")
+    
     display = {
       newGroup = function()
-      return fakeFloorGroup
+        return fakeFloorGroup
       end
     }
     spy.on(display, "newGroup")
-    
-    FloorChunkBuilder = {}
-    stub(FloorChunkBuilder, "buildFromPatternAndVerticalOffset")
+
+    FloorChunkBuilder = {
+      buildFromPatternAndVerticalOffset = function()
+        return fakeCreatedFloorChunk
+      end
+    }
+    spy.on(FloorChunkBuilder, "buildFromPatternAndVerticalOffset")
 
     FloorPatterns = {
       {"body", "top"}
@@ -18,18 +29,24 @@ describe("FloorBuilder", function()
 
     FloorBuilder = require("scripts.FloorBuilder")
   end)
-  
+
   it("should create a floor group", function()
     FloorBuilder.build()
-    
+
     assert.stub(display.newGroup).was_called()
   end)
-  
+
   it("should create floor chunk for 16 times", function()
     FloorBuilder.build()
 
     for expectedOffset = 1,16 do
       FloorChunkBuilder.buildFromPatternAndVerticalOffset({"body", "top"}, expectedOffset)
     end
+  end)
+
+  it("should insert created floor chunk into floor group", function()
+    FloorBuilder.build()
+
+    assert.stub(fakeFloorGroup.insert).was_called_with(fakeFloorGroup, fakeCreatedFloorChunk)
   end)
 end)
