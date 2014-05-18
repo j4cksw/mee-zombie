d8604df8d710319061c38077b3f1ba44ff833069
j4cksw <ninjaadvance@gmail.com>
2014-05-18 16:18:58 +0700

    Create new group in FloorBuilder

--- scripts/FloorBuilder.lua
+++ scripts/FloorBuilder.lua
@@ -4,6 +4,8 @@ FloorChunkBuilder = FloorChunkBuilder or require("scripts.FloorChunkBuilder")
 FloorPatterns = FloorPatterns or require("config.FloorPatterns")
 
 function FloorBuilder.build()
+  display.newGroup()
+  
   for horizontalOffset = 1,16 do
     FloorChunkBuilder.buildFromPatternAndVerticalOffset(FloorPatterns[1],
       horizontalOffset)
--- spec/FloorBuilder_spec.lua
+++ spec/FloorBuilder_spec.lua
@@ -2,6 +2,13 @@ describe("FloorBuilder", function()
   local FloorBuilder
 
   setup(function()
+    display = {
+      newGroup = function()
+      return fakeFloorGroup
+      end
+    }
+    spy.on(display, "newGroup")
+    
     FloorChunkBuilder = {}
     stub(FloorChunkBuilder, "buildFromPatternAndVerticalOffset")
 
@@ -11,7 +18,13 @@ describe("FloorBuilder", function()
 
     FloorBuilder = require("scripts.FloorBuilder")
   end)
-
+  
+  it("should create a floor group", function()
+    FloorBuilder.build()
+    
+    assert.stub(display.newGroup).was_called()
+  end)
+  
   it("should create floor chunk for 16 times", function()
     FloorBuilder.build()
 
