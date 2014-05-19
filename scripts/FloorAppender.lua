d4fb1d2dfe78f8623c3d8d60fe3fe2d7d3d77afb
j4cksw <ninjaadvance@gmail.com>
2014-05-19 22:02:50 +0700

    Implemented FloorAppender

--- scripts/FloorAppender.lua
+++ scripts/FloorAppender.lua
@@ -1,6 +1,11 @@
 local FloorAppender = {}
 
+FloorRepository = FloorRepository or require("scripts.FloorRepository")
+FloorChunkBuilder = FloorChunkBuilder or require("scripts.FloorChunkBuilder")
+
 function FloorAppender.append()
+  local floorGroup = FloorRepository.getFloorGroup()
+  floorGroup:insert(FloorChunkBuilder.buildFromPatternAndVerticalOffset({"body", "top"}, 5))
 end
 
 return FloorAppender
--- spec/FloorAppender_spec.lua
+++ spec/FloorAppender_spec.lua
@@ -1,13 +1,52 @@
 describe("FloorAppender", function()
   local FloorAppender
-  
+
+  local FloorDataGenerator = require("spec.lib.FloorDataGenerator")
+
+  local fakeCreatedFloorChunk = {}
+
   setup(function()
+    FloorRepository = {
+      getFloorGroup = function()
+        return Floor
+      end
+    }
+    spy.on(FloorRepository, "getFloorGroup")
+
+    FloorChunkBuilder = {
+      buildFromPatternAndVerticalOffset = function(...)
+        return fakeCreatedFloorChunk
+      end
+    }
+    spy.on(FloorChunkBuilder, "buildFromPatternAndVerticalOffset")
+
     FloorAppender = require("scripts.FloorAppender")
   end)
-  
-  it("should append new floor chunk to the floor", function()
+
+  it("should acquire floor group from FloorRepository", function()
+    FloorDataGenerator.setupFloorData(5)
+    stub(Floor, "insert")
+
     FloorAppender.append()
-    
+
+    assert.stub(FloorRepository.getFloorGroup).was_called()
+  end)
+
+  it("should create new Floor chunk", function()
+    FloorDataGenerator.setupFloorData(5)
+    stub(Floor, "insert")
+
+    FloorAppender.append()
+
+    assert.stub(FloorChunkBuilder.buildFromPatternAndVerticalOffset).was_called_with({"body", "top"}, 5)
+  end)
+
+  it("should append new floor chunk to the floor", function()
+    FloorDataGenerator.setupFloorData(5)
+    stub(Floor, "insert")
+
+    FloorAppender.append()
+
     assert.stub(Floor.insert).was_called_with(Floor, fakeCreatedFloorChunk)
   end)
 end)
