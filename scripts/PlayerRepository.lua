4c9fe2c4db52db06af3b854a345de6c8d752a7af
j4cksw <ninjaadvance@gmail.com>
2014-05-20 11:58:23 +0700

    Implemented PlayerRepository

---
+++ scripts/PlayerRepository.lua
@@ -0,0 +1,13 @@
+local PlayerRepository = {}
+
+PlayerRepository.playerSprite = nil
+
+function PlayerRepository.setPlayerSprite(sprite)
+  PlayerRepository.playerSprite = sprite
+end
+
+function PlayerRepository.getPlayerSprite()
+  return PlayerRepository.playerSprite
+end
+
+return PlayerRepository
--- spec/PlayerRepository_spec.lua
+++ spec/PlayerRepository_spec.lua
@@ -1,16 +1,16 @@
 describe("PlayerRepository", function()
   local PlayerRepository
-  
+
   setup(function()
-    local PlayerRepository = require("scripts.PlayerRepository")
+    PlayerRepository = require("scripts.PlayerRepository")
   end)
-  
+
   it("should return stored player sprite", function()
     local fakePlayerSprite = {}
     PlayerRepository.setPlayerSprite(fakePlayerSprite)
-    
+
     local result = PlayerRepository.getPlayerSprite()
-    
+
     assert.are.equal(result, fakePlayerSprite)
   end)
 end)
