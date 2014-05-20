9782b1714ccc4bec780b67a2a5cd5670889483de
j4cksw <ninjaadvance@gmail.com>
2014-05-20 11:24:23 +0700

    Add PlayerAttackCommand

--- spec/PlayerAttackCommand_spec.lua
+++ spec/PlayerAttackCommand_spec.lua
@@ -2,10 +2,17 @@ describe("PlayerAttackCommand", function()
   local PlayerAttackCommand
   
   setup(function()
+    SpriteSequenceTransition = {}
+    stub(SpriteSequenceTransition, "toSequence")
+    
     PlayerAttackCommand = require("scripts.PlayerAttackCommand")
   end)
   
-  it("should change player sequence to 'attack'")
+  it("should change player sequence to 'attack'", function()
+    PlayerAttackCommand.execute()
+    
+    assert.stub(SpriteSequenceTransition.toSequence).was_called_with("attack")
+  end)
   
   it("should add sprite event listener to player sprite")
 end)
