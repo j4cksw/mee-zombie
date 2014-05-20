203aa0657b5644600fb22ce14309e26e104841a0
j4cksw <ninjaadvance@gmail.com>
2014-05-20 16:26:20 +0700

    Fixed missing require in GameInitializer

--- scripts/GameInitializer.lua
+++ scripts/GameInitializer.lua
@@ -2,8 +2,8 @@ local GameInitializer = {}
 
 ImageSheetLoader = ImageSheetLoader or require("scripts.ImageSheetLoader")
 FloorBuilder = FloorBuilder or require("scripts.FloorBuilder")
-GameInitializeData = GameInitializeData or require("config.GameInitializeData")
 FloorRoller = FloorRoller or require("scripts.FloorRoller")
+PlayerInitializer = PlayerInitializer or require("scripts.PlayerInitializer")
 
 function GameInitializer.initialize()
   ImageSheetLoader.loadByNames({"bear_zombie", "floor"})
