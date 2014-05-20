ff8560b2b937dbcbab54660f9dc31dba4e5805f7
j4cksw <ninjaadvance@gmail.com>
2014-05-20 15:14:20 +0700

    Prevent PlayerAttackEndedListener from other events

--- scripts/PlayerAttackEndedListener.lua
+++ scripts/PlayerAttackEndedListener.lua
@@ -3,9 +3,14 @@ local PlayerAttackEndedListener = {}
 PlayerRepository = PlayerRepository or require("scripts.PlayerRepository")
 SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
 function PlayerAttackEndedListener.actionPerformed(event)
+
+  if event.phase ~= "ended" then
+    return
+  end
+
   local playerSprite = PlayerRepository.getPlayerSprite()
   playerSprite:removeEventListener("sprite", PlayerAttackEndedListener.actionPerformed)
-  
+
   SpriteSequenceTransition.toSequence(playerSprite, "walk")
 end
 
