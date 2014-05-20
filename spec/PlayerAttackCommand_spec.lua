cb9259ad24384c3a1c387d515f3d6f6886fd29ed
j4cksw <ninjaadvance@gmail.com>
2014-05-20 11:20:44 +0700

    Add PlayerAttackCommand_spec

---
+++ spec/PlayerAttackCommand_spec.lua
@@ -0,0 +1,11 @@
+describe("PlayerAttackCommand", function()
+  local PlayerAttackCommand
+  
+  setup(function()
+    PlayerAttackCommand = require("scripts.PlayerAttackCommand")
+  end)
+  
+  it("should change player sequence to 'attack'")
+  
+  it("should add sprite event listener to player sprite")
+end)
