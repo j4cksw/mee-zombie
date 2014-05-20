5b67e30f887836da66eb4ff1bb210bec7c293760
j4cksw <ninjaadvance@gmail.com>
2014-05-20 15:42:20 +0700

    Implemented PlayerWalkCommand

--- scripts/PlayerWalkCommand.lua
+++ scripts/PlayerWalkCommand.lua
@@ -1,2 +1,14 @@
 PlayerWalkCommand = {}
+
+PlayerRepository = PlayerRepository or require("scripts.PlayerRepository")
+SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
+PlaterAttackCommand = PlayerAttackCommand or require("scripts.PlayerAttackCommand")
+
+function PlayerWalkCommand.execute()
+  local playerSprite = PlayerRepository.getPlayerSprite()
+  SpriteSequenceTransition.toSequence(playerSprite, "walk")
+  
+  Runtime:addEventListener("tap", PlaterAttackCommand.execute)
+end
+
 return PlayerWalkCommand
