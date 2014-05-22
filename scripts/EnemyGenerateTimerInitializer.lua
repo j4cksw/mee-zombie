342a50e27dc1cf221b4d2c65bad35b6166f3977f
j4cksw <ninjaadvance@gmail.com>
2014-05-22 12:41:04 +0700

    Add created enemyGroup to EnemyRepository

--- scripts/EnemyGenerateTimerInitializer.lua
+++ scripts/EnemyGenerateTimerInitializer.lua
@@ -1,8 +1,12 @@
 EnemyGenerateTimerInitializer = {}
 
 EnemyInitializer = EnemyInitializer or require("scripts.EnemyInitializer")
+EnemyRepository = EnemyRepository or require("scripts.EnemyRepository")
 
 function EnemyGenerateTimerInitializer.initialize()
+  local enemyGroup = display.newGroup()
+  EnemyRepository.setEnemyGroup(enemyGroup)
+  
   timer.performWithDelay(500, EnemyInitializer.initialize, 0)
 end
 
--- spec/EnemyGeneratorInitialzer_spec.lua
+++ spec/EnemyGeneratorInitialzer_spec.lua
@@ -1,20 +1,43 @@
 describe("EnemyGenerateTimerInitializer", function()
-  it("should start timer for generating enemies", function()
+  local EnemyGenerateTimerInitializer
+  local fakeEnemyGroup = {}
+
+  setup(function()
     timer = {}
     stub(timer, "performWithDelay")
-    
+
     EnemyInitializer = {
       initialize = function()end
     }
+
+    display = {
+      newGroup = function()
+        return fakeEnemyGroup
+      end
+    }
+    spy.on(display, "newGroup")
     
-    local EnemyGenerateTimerInitializer = require("scripts.EnemyGenerateTimerInitializer")
-    
+    EnemyRepository = {}
+    stub(EnemyRepository, "setEnemyGroup")
+
+    EnemyGenerateTimerInitializer = require("scripts.EnemyGenerateTimerInitializer")
+  end)
+
+  it("should start timer for generating enemies", function()
     EnemyGenerateTimerInitializer.initialize()
-    
+
     assert.stub(timer.performWithDelay).was_called_with(500, EnemyInitializer.initialize, 0)
   end)
-  
-  it("should create enemy group")
-  
-  it("should set enemy group to EnemyRepository")
+
+  it("should create enemy group", function()
+    EnemyGenerateTimerInitializer.initialize()
+
+    assert.stub(display.newGroup).was_called()
+  end)
+
+  it("should set enemy group to EnemyRepository", function()
+    EnemyGenerateTimerInitializer.initialize()
+    
+    assert.stub(EnemyRepository.setEnemyGroup).was_called_with(fakeEnemyGroup)
+  end)
 end)
