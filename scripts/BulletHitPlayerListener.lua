2dc621e29aabf632de9c7ddb9ea0ef5f7606883b
j4cksw <ninjaadvance@gmail.com>
2014-05-25 12:57:02 +0700

    Remove bullet when hit the player sprite

---
+++ scripts/BulletHitPlayerListener.lua
@@ -0,0 +1,9 @@
+BulletHitPlayerListener = {}
+
+function BulletHitPlayerListener.actionPerformed(event)
+  if event.other.type == "player" then
+    event.target.removeSelf()
+  end
+end
+
+return BulletHitPlayerListener
--- spec/BulletHitPlayerListener_spec.lua
+++ spec/BulletHitPlayerListener_spec.lua
@@ -1,19 +1,22 @@
 describe("BulletHitPlayerListener", function()
   local BulletHitPlayerListener
-  
+
   local fakeTargetBulletSprite = {}
-  
+
   local event = {
+    other = {
+      type = "player"
+    },
     target = fakeTargetBulletSprite
   }
-  
+
   it("should remove bullet itself when hit player", function()
     stub(fakeTargetBulletSprite, "removeSelf")
-    
+
     BulletHitPlayerListener = require("scripts.BulletHitPlayerListener")
-    
+
     BulletHitPlayerListener.actionPerformed(event)
-    
+
     assert.stub(fakeTargetBulletSprite.removeSelf).was_called()
   end)
 end)
