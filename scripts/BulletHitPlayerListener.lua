f4885457166d9af1226a84a0da01aaf9eb55cdc1
j4cksw <ninjaadvance@gmail.com>
2014-05-25 14:00:39 +0700

    Change sprite to dead when bullet hit the player

--- scripts/BulletHitPlayerListener.lua
+++ scripts/BulletHitPlayerListener.lua
@@ -1,8 +1,11 @@
 BulletHitPlayerListener = {}
 
+SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
+
 function BulletHitPlayerListener.actionPerformed(event)
   if event.other.type == "player" then
     event.target:removeSelf()
+    SpriteSequenceTransition.toSequence(event.other, "dead")
   end
 end
 
--- spec/BulletHitPlayerListener_spec.lua
+++ spec/BulletHitPlayerListener_spec.lua
@@ -9,14 +9,25 @@ describe("BulletHitPlayerListener", function()
     },
     target = fakeTargetBulletSprite
   }
-
-  it("should remove bullet itself when hit player", function()
+  
+  setup(function()
     stub(fakeTargetBulletSprite, "removeSelf")
+    
+    SpriteSequenceTransition = {}
+    stub(SpriteSequenceTransition, "toSequence")
 
     BulletHitPlayerListener = require("scripts.BulletHitPlayerListener")
+  end)
 
+  it("should remove bullet itself when hit player", function()
     BulletHitPlayerListener.actionPerformed(event)
 
     assert.stub(fakeTargetBulletSprite.removeSelf).was_called_with(fakeTargetBulletSprite)
+  end)
+  
+  it("player should dead", function()
+    BulletHitPlayerListener.actionPerformed(event)
+    
+    assert.stub(SpriteSequenceTransition.toSequence).was_called_with(event.other, "dead")
   end)
 end)
