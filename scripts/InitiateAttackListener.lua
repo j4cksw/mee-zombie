dec17f4940a57aa57a920f10876ba9f629003283
j4cksw <ninjaadvance@gmail.com>
2014-05-25 15:18:18 +0700

    Create rectangle to use as a slash region in InitiateAttackListener

--- scripts/InitiateAttackListener.lua
+++ scripts/InitiateAttackListener.lua
@@ -6,9 +6,17 @@ function InitiateAttackListener.actionPerformed(event)
   if event.target.frame < 5 then
     return
   end
+
+  local slashRect = display.newRect(event.target.x+100,
+    event.target.y,
+    200,
+    400)
+  slashRect.alpha = 0
+  slashRect.type = "slash"
+  physics.addBody(slashRect, "dynamic")
   
-  physics.addBody(event.target, "dynamic", {isSensor=true})
-  event.target.gravityScale = 0
+  event.target.slash = slashRect
+  
   event.target:removeEventListener("sprite", InitiateAttackListener.actionPerformed)
 end
 
--- spec/InitiateAttackListener_spec.lua
+++ spec/InitiateAttackListener_spec.lua
@@ -3,25 +3,41 @@ describe("InitiateAttackListener", function()
   
   local event = {
     target={
-      frame=5
+      frame=5,
+      x=500,
+      y=2000
     }
   }
+  
+  local fakeSlashRect = {}
   
   setup(function()
     physics = {}
     stub(physics, "addBody")
     
     stub(event.target, "removeEventListener")
-  
+    
+    display = {
+      newRect = function()
+        return fakeSlashRect
+      end
+    }
+    spy.on(display, "newRect")
+    
     InitiateAttackListener = require("scripts.InitiateAttackListener")
   end)
   
+  it("should create rectangle for use as slash body", function()
+    InitiateAttackListener.actionPerformed(event)
+    
+    assert.stub(display.newRect).was_called_with(600, 2000, 200, 400)
+  end)
   it("should add physics body when reach the 4th frame", function()
     InitiateAttackListener.actionPerformed(event)
     
-    assert.stub(physics.addBody).was_called_with(event.target, "dynamic", {isSensor=true})
+    assert.stub(physics.addBody).was_called_with(fakeSlashRect, "dynamic")
   end)
-  
+   
   it("should remove itself from player sprite event listener", function()
     InitiateAttackListener.actionPerformed(event)
     
