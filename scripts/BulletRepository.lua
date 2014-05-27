c74d80c947172d31347a10a19f692b59d58a531f
j4cksw <ninjaadvance@gmail.com>
2014-05-27 22:03:32 +0700

    Insert given bullet sprite to bullet group

--- scripts/BulletRepository.lua
+++ scripts/BulletRepository.lua
@@ -5,6 +5,7 @@ BulletRepository.bulletGroup = nil
 function BulletRepository.insert(bulletSprite)
   if not BulletRepository.bulletGroup then
     BulletRepository.bulletGroup = display.newGroup()
+    BulletRepository.bulletGroup:insert(bulletSprite)
   end
 end
 
--- spec/BulletRepository_spec.lua
+++ spec/BulletRepository_spec.lua
@@ -1,5 +1,8 @@
 describe("BulletRepository", function()
-
+    
+    local fakeGroup = {}
+    local fakeBullet = {}
+    
     setup(function()
       display = {
         newGroup = function()
@@ -7,17 +10,23 @@ describe("BulletRepository", function()
         end
       }
       spy.on(display, "newGroup")
+      
+      stub(fakeGroup, "insert")
+      
+      BulletRepository = require("scripts.BulletRepository")
     end)
 
     it("should create group if not available when insert", function()
-      BulletRepository = require("scripts.BulletRepository")
-
-      BulletRepository.insert()
+      BulletRepository.insert(fakeBullet)
 
       assert.stub(display.newGroup).was_called()
     end)
 
-    it("should insert given bullet sprite to group when insert")
+    it("should insert given bullet sprite to group when insert", function()
+      BulletRepository.insert(fakeBullet)
+      
+      assert.stub(fakeGroup.insert).was_called_with(fakeGroup, fakeBullet)
+    end)
 
     it("should remove group when removeAll")
 end)
