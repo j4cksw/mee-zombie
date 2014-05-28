4d9b245b94feb2143558e322773a6a32042c1ff4
j4cksw <ninjaadvance@gmail.com>
2014-05-28 22:17:36 +0700

    insert initialized item sprite to ITemREpository

--- scripts/ItemInitializer.lua
+++ scripts/ItemInitializer.lua
@@ -1,14 +1,17 @@
 ItemInitializer = {}
 
 SpriteInitializer = SpriteInitializer or require("scripts.SpriteInitializer")
+ItemRepository = ItemRepository or require("scripts.ItemRepository")
 
 function ItemInitializer.initialize(sourceSprite)
-  SpriteInitializer.initializeByData({
+  local itemSprite = SpriteInitializer.initializeByData({
     name="item",
     x=sourceSprite.x,
     y=sourceSprite.y-250,
     sequence="item"
   })
+  
+  ItemRepository.insert(itemSprite)
 end
 
 return ItemInitializer
--- spec/ItemInitializer_spec.lua
+++ spec/ItemInitializer_spec.lua
@@ -1,30 +1,46 @@
 describe("ItemInitializer", function()
-  it("should create item sprite at top of dead enemy", function()
+
     local fakeEnemySprite = {
       x=1000,
       y=2000
     }
     
-    SpriteInitializer = {}
-    stub(SpriteInitializer, "initializeByData")
-    
-    ItemInitializer = require("scripts.ItemInitializer")
+    local fakeItemSprite = {}
 
-    ItemInitializer.initialize(fakeEnemySprite)
+    setup(function()
+      SpriteInitializer = {
+        initializeByData = function()
+          return fakeItemSprite
+        end
+      }
+      spy.on(SpriteInitializer, "initializeByData")
+      
+      ItemRepository = {}
+      stub(ItemRepository, "insert")
 
-    assert.stub(SpriteInitializer.initializeByData).was_called_with({
-      name="item",
-      x=1000,
-      y=1750,
-      sequence="item"
-    })
-  end)
-  
-  it("should insert to ItemRepository")
-  
-  it("should add physics body to item")
-  
-  it("should fly the item")
+      ItemInitializer = require("scripts.ItemInitializer")
+    end)
 
-  it("should add PlayerHitItemListener to item")
+    it("should create item sprite at top of dead enemy", function()
+      ItemInitializer.initialize(fakeEnemySprite)
+
+      assert.stub(SpriteInitializer.initializeByData).was_called_with({
+        name="item",
+        x=1000,
+        y=1750,
+        sequence="item"
+      })
+    end)
+
+    it("should insert to ItemRepository", function()
+      ItemInitializer.initialize(fakeEnemySprite)
+      
+      assert.stub(ItemRepository.insert).was_called_with(fakeItemSprite)
+    end)
+
+    it("should add physics body to item")
+
+    it("should fly the item")
+
+    it("should add PlayerHitItemListener to item")
 end)
