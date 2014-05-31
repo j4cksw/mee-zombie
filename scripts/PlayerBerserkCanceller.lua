ce1f26a75827a678c1765da94cb0672d74a69782
j4cksw <ninjaadvance@gmail.com>
2014-05-31 23:39:13 +0700

    set player state to walk when berserk canceled

--- scripts/PlayerBerserkCanceller.lua
+++ scripts/PlayerBerserkCanceller.lua
@@ -1,8 +1,12 @@
 PlayerBerserkCanceller = {}
+PlayerRepository = PlayerRepository or require("scripts.PlayerRepository")
+SpriteSequenceTransition = SpriteSequenceTransition or require("scripts.SpriteSequenceTransition")
 
 function PlayerBerserkCanceller.cancel()
   AttackRect:removeSelf()
-
+  
+  local playerSprite = PlayerRepository.getPlayerSprite()
+  SpriteSequenceTransition.toSequence(playerSprite, "walk")
 end
 
 return PlayerBerserkCanceller
--- spec/PlayerBerserkCanceller_spec.lua
+++ spec/PlayerBerserkCanceller_spec.lua
@@ -1,16 +1,42 @@
 describe("PlayerBerserkCanceller", function()
-  it("should remove attack rectangle", function()
+  local PlayerBerserkCanceller
+  
+  local fakePlayerSprite = {}
+  
+  setup(function()
     AttackRect = {}
     stub(AttackRect, "removeSelf")
     
-    PlayerBerserkCanceller = require("scripts.PlayerBerserkCanceller")
+    PlayerRepository = {
+      getPlayerSprite = function()
+        return fakePlayerSprite
+      end
+    }
+    spy.on(PlayerRepository, "getPlayerSprite")
     
+    SpriteSequenceTransition = {}
+    stub(SpriteSequenceTransition, "toSequence")
+    
+    PlayerBerserkCanceller = require("scripts.PlayerBerserkCanceller")
+  end)
+
+  it("should remove attack rectangle", function()
     PlayerBerserkCanceller.cancel()
     
     assert.stub(AttackRect.removeSelf).was_called_with(AttackRect)
   end)
   
-  it("should set player state back to walking")
+  it("shpuld get player form PlayerRepository", function()
+    PlayerBerserkCanceller.cancel()
+    
+    assert.stub(PlayerRepository.getPlayerSprite).was_called()
+  end)
+  
+  it("should set player state back to walking", function()
+    PlayerBerserkCanceller.cancel()
+    
+    assert.stub(SpriteSequenceTransition.toSequence).was_called_with(fakePlayerSprite, "walk")
+  end)
   
   it("should set game speed back to previous speed")
 end)
