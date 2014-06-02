e3dfe25c8b8831dee21b226a6f570f0c7e37b083
j4cksw <ninjaadvance@gmail.com>
2014-06-02 07:22:11 +0700

    refactored FloorGapRemoveCommand add function getRightEdgePosition

--- scripts/FloorGapRemoveCommand.lua
+++ scripts/FloorGapRemoveCommand.lua
@@ -6,12 +6,16 @@ function getLeftEdgePosition(sprite)
   return sprite.x - (sprite.width/2)
 end
 
+function getRightEdgePosition(sprite)
+  return sprite.x + (sprite.width/2)
+end
+
 function FloorGapRemoveCommand.execute()
   local floorGroup = FloorRepository.getFloorGroup()
 
   for chunkIndex = 2, floorGroup.numChildren do
     for pieceIndex = 1, floorGroup[chunkIndex].numChildren do
-      if getLeftEdgePosition(floorGroup[chunkIndex][pieceIndex])> ((floorGroup[1][1].x + (floorGroup[1][1].x/2))+1) then
+      if getLeftEdgePosition(floorGroup[chunkIndex][pieceIndex])> getRightEdgePosition(floorGroup[chunkIndex-1][pieceIndex]) then
         floorGroup[2][1].x = ((floorGroup[1][1].x + (floorGroup[1][1].width/2))+1) + (floorGroup[2][1].width/2)
       end
     end
