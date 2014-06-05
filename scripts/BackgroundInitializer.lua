3489375d65110e594842f180aec7123d805c2ed6
j4cksw <ninjaadvance@gmail.com>
2014-06-05 21:26:30 +0700

    change from hardcode position of background to be value from display module

--- scripts/BackgroundInitializer.lua
+++ scripts/BackgroundInitializer.lua
@@ -5,8 +5,8 @@ SpriteInitializer = SpriteInitializer or require("scripts.SpriteInitializer")
 function BackgroundInitializer.initialize()
   SpriteInitializer.initializeByData({
     name="background",
-    x=500,
-    y=600,
+    x=display.contentCenterX,
+    y=display.contentCenterY,
     sequence="background"
   })
 end
