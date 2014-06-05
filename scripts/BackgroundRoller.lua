d98246f38cd2ea469c4405c6e662039d0692945a
j4cksw <ninjaadvance@gmail.com>
2014-06-05 22:09:53 +0700

    move background sprite by  constant value

--- scripts/BackgroundRoller.lua
+++ scripts/BackgroundRoller.lua
@@ -3,8 +3,9 @@ BackgroundRoller = {}
 BackgroundRepository = BackgroundRepository or require("scripts.BackgroundRepository")
 
 function BackgroundRoller.roll()
-  BackgroundRepository.getBackgroundGroup()
+  local backgroundGroup = BackgroundRepository.getBackgroundGroup()
   
+  backgroundGroup[1].x = backgroundGroup[1].x - 8
 end
 
 return BackgroundRoller
--- spec/BackgroundRoller_spec.lua
+++ spec/BackgroundRoller_spec.lua
@@ -1,23 +1,37 @@
 describe("BackgroundRoller", function()
   local BackgroundRoller
-  
-  
+
+  local fakeBackgroundGroup
+
   setup(function()
-    BackgroundRepository = {}
-    stub(BackgroundRepository, "getBackgroundGroup")
-    
+    BackgroundRepository = {
+      getBackgroundGroup = function()
+        return fakeBackgroundGroup
+      end
+    }
+    spy.on(BackgroundRepository, "getBackgroundGroup")
+
     BackgroundRoller = require("scripts.BackgroundRoller")
   end)
-  
+
+  before_each(function()
+    fakeBackgroundGroup = {
+      {
+        x=0
+      },
+      numChildren=1
+    }
+  end)
+
   it("should get background from BackgroundRepository", function()
     BackgroundRoller.roll()
-    
+
     assert.stub(BackgroundRepository.getBackgroundGroup).was_called()
   end)
-  
+
   it("should move background sprite with constant speed", function()
     BackgroundRoller.roll()
-    
-    assert.are.equal(fakeBackgroundSprite.x, -8)
+
+    assert.are.equal(fakeBackgroundGroup[1].x, -8)
   end)
 end)
