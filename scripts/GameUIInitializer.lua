d88a00b9c0832ee1ceff7155786faceaad2ff53e
j4cksw <ninjaadvance@gmail.com>
2014-06-10 22:17:35 +0700

    create rave level sprite in GameUIInitializer

--- scripts/GameUIInitializer.lua
+++ scripts/GameUIInitializer.lua
@@ -1,6 +1,14 @@
 GameUIInitializer = {}
 
+SpriteInitializer = SpriteInitializer or require("scripts.SpriteInitializer")
+
 function GameUIInitializer.initialize()
+  SpriteInitializer.initializeByData({
+      name="rave_level",
+      x=-500,
+      y=48,
+      sequence="rave_level_1"
+    })
   display.newText("0", -500, 48, "boon-500", 72)
 end
 
--- spec/GameUIInitializer_spec.lua
+++ spec/GameUIInitializer_spec.lua
@@ -1,14 +1,30 @@
 describe("GameUIInitializer", function()
   local GameUIInitializer
-  
-  it("should create score", function()
+
+  setup(function()
     display = {}
     stub(display, "newText")
     
+    SpriteInitializer = {}
+    stub(SpriteInitializer, "initializeByData")
+
     GameUIInitializer = require("scripts.GameUIInitializer")
-    
+  end)
+
+  it("should create rave level sprite", function()
     GameUIInitializer.initialize()
     
+    assert.stub(SpriteInitializer.initializeByData).was_called_with({
+      name="rave_level",
+      x=-500,
+      y=48,
+      sequence="rave_level_1"
+    })
+  end)
+
+  it("should create score", function()
+    GameUIInitializer.initialize()
+
     assert.stub(display.newText).was_called_with("0", -500, 48, "boon-500", 72)
   end)
 end)
