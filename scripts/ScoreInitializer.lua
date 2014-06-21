640afb96870c7b39e4bf36cce63dbb3bd9d98c2d
j4cksw <ninjaadvance@gmail.com>
2014-06-21 14:36:12 +0700

    Set score to 0 when initializing score

--- scripts/ScoreInitializer.lua
+++ scripts/ScoreInitializer.lua
@@ -1,6 +1,7 @@
 ScoreInitializer = {}
 
 function ScoreInitializer.initialize()
+    CurrentScore = 0
     display.newText(CurrentScore, -300, 128, "Bookshelf", 72)
 end
 return ScoreInitializer
--- spec/ScoreInitializer_spec.lua
+++ spec/ScoreInitializer_spec.lua
@@ -1,12 +1,25 @@
 describe("ScoreInitializer", function()
-    it("should create score text", function()
+
+    setup(function()
         display = {
             newText = function () end
         }
         stub(display, "newText")
 
         ScoreInitializer = require("scripts.ScoreInitializer")
+    end)
 
+    before_each(function()
+        CurrentScore = nil
+    end)
+
+    it("should set CurrentScore to 0", function()
+        ScoreInitializer.initialize()
+
+        assert.are.equal(CurrentScore, 0)
+    end)
+
+    it("should create score text", function()
         ScoreInitializer.initialize()
 
         assert.stub(display.newText).was_called_with(CurrentScore, -300, 128, "Bookshelf", 72)
