5479de9429eed48ad336412ddfdb35fef0f89c74
j4cksw <ninjaadvance@gmail.com>
2014-10-11 21:16:56 +0700

    add get() to AudioRepository

--- scripts/AudioRepository.lua
+++ scripts/AudioRepository.lua
@@ -6,4 +6,8 @@ function AudioRepository.add(name, value)
     AudioRepository.data[name] = value
 end
 
+function AudioRepository.get(name)
+    return AudioRepository.data[name]
+end
+
 return AudioRepository
--- spec/AudioRepository_spec.lua
+++ spec/AudioRepository_spec.lua
@@ -10,4 +10,14 @@ describe("AudioRepository", function()
 
         assert.are.equal(AudioRepository.data["game_bgm"], fakeAudioInstance)
     end)
+
+    it("get() should return audio instance indexed by a given key", function()
+        AudioRepository = require("scripts/AudioRepository")
+
+        local fakeAudioInstance = {}
+
+        AudioRepository.add("game_bgm", fakeAudioInstance)
+
+        assert.are.equal(AudioRepository.get("game_bgm"), fakeAudioInstance)
+    end)
 end)
